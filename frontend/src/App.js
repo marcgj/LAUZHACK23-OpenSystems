aff72ff6cb1f926deaef1fc02becd3ebe7f312ce
marcgj <mgaspjoval2002@gmail.com>
2023-12-02 18:25:34 +0100

    send file done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import "./pages/fileUpload";
+import FileUpload from "./pages/fileUpload";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Lalala</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element={<FileUpload />} />
+          <Route path="/chat" element={"WIP"}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
