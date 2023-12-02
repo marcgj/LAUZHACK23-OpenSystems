6bd0f154c2e9e75949927a975bcf565b33cd4845
rafelsalgueiro <rss18@alumnes.udl.cat>
2023-12-03 00:22:34 +0100

    chat page

--- frontend/src/pages/chatPage.js
+++ frontend/src/pages/chatPage.js
@@ -1,29 +1,44 @@
+import React from "react";
 
-import React, { useState } from 'react';
-
-const ChatPage = () => {
-  const [inputText, setInputText] = useState('');
-  const [displayText, setDisplayText] = useState('');
-
-  const handleInputChange = (e) => {
-    setInputText(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setDisplayText(inputText);
-    setInputText('');
-  };
-
+export default function Chat() {
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={inputText} onChange={handleInputChange} />
-        <button type="submit">Send</button>
-      </form>
-      <p>{displayText}</p>
-    </div>
+    <>
+      <div className="w-2/3 m-auto">
+        <div
+          style={{ height: "70vh" }}
+          className="flex flex-col my-auto bg-white/30 p-3 backdrop-blur-sm rounded-t overflow-scroll"
+        >
+          <div className="flex row items-center p-3 my-2  shadow-md max-w-lg rounded bg-black/70">
+            <img
+              src="user.png"
+              className="inline aspect-square h-10 rounded-full bg-white/80 p-1 mx-2"
+              alt=""
+            />
+            <span>
+              La mamam de la ama de la mama de la mamaLa mamam de la ama de la
+              mama de la mama La mamam de la ama de la mama de la mama
+            </span>
+          </div>
+          <div className="flex flex-row-reverse place-self-end  shadow-md  items-center p-3 my-2 max-w-lg rounded bg-black/70">
+            <img
+              src="user.png"
+              className="inline aspect-square h-10 rounded-full bg-white/80 p-1 mx-2"
+              alt=""
+            />
+            <span>
+              La mamam de la ama de la mama de la mamaLa mamam de la ama de la
+              mama de la mama La mamam de la ama de la mama de la mama
+            </span>
+          </div>
+        </div>
+        <input
+          type="text"
+          className="p-3 w-full rounded-b"
+          placeholder="Write your prompt"
+          name=""
+          id=""
+        />
+      </div>
+    </>
   );
-};
-
-export default ChatPage;
+}
