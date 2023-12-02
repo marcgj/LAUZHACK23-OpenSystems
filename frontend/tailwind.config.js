9ca685c718c7547b3fe1ad567f0c2667074fc545
marcgj <mgaspjoval2002@gmail.com>
2023-12-02 16:15:21 +0100

    fix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,7 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  content: ["./src/**/*.{js}"],
+  content: ["*.js"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
