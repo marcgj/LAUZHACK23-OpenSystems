cb58e644062b681f77d46b4d6454f91ead711806
marcgj <mgaspjoval2002@gmail.com>
2023-12-02 16:01:51 +0100

    fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
-import fileUpload from "./pages/fileUpload";
-
 function App() {
-  return (
-    <div className="App">
-      <fileUpload>npm install -D tailwindcss npx tailwindcss init</fileUpload>
-    </div>
-  );
+  return <div className="App">Lalala</div>;
 }
 
 export default App;
